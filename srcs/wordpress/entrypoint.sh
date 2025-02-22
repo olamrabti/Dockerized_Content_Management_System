e81bbcabec28cbaffd6746c5ac6ecbab823c83fd
olamrabti <oussamalamrabtii@gmail.com>
2025-02-22 19:28:19 +0100

    all is setup and working except wp config

--- srcs/wordpress/entrypoint.sh
+++ srcs/wordpress/entrypoint.sh
@@ -1,14 +1,12 @@
 #!/bin/bash
-
 # Wait for MariaDB to be ready
 echo "Waiting for database connection..."
-# until mariadb -h"mariadb" -u"${MYSQL_USER}" -p"${MYSQL_PASSWORD}" -e "SELECT 1;" &>/dev/null; do
-# until mariadb -h"mariadb" -u"wp_user" -p"1234" -e "SELECT 1;" &>/dev/null; do
-  sleep 5
+sleep 10
+# mariadb -h"${WORDPRESS_DB_HOST}" -u"${WORDPRESS_DB_USER}" -p"${WORDPRESS_DB_PASSWORD}" -e "SELECT 1;";
+# mariadb -h"mariadb" -u"wp_user" -p"1234" -e "SELECT 1;"
 # done
 # echo "Database ready!"
 
-#---------------------------------------------------wp installation---------------------------------------------------#
 # wp-cli installation
 curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar
 # wp-cli permission
@@ -25,23 +23,21 @@ cd /var/www/wordpress
 chmod -R 755 /var/www/wordpress/
 # change owner of wordpress directory to www-data
 chown -R www-data:www-data /var/www/wordpress
-#---------------------------------------------------wp installation---------------------------------------------------##---------------------------------------------------wp installation---------------------------------------------------#
 
 if [ ! -f /var/www/wordpress/wp-config.php ]; then
     echo "Installing WordPress..."
     wp core download  --allow-root
     # create wp-config.php file with database details
-    wp config create --dbhost="mariadb" --dbname="wordpress" --dbuser="wp_user" --dbpass="1234" --allow-root
+    wp config create --dbhost="${WORDPRESS_DB_HOST}" --dbname="${WORDPRESS_DB_NAME}" --dbuser="${WORDPRESS_DB_USER}" --dbpass="${WORDPRESS_DB_PASSWORD}" --allow-root
     # install wordpress with the given title, admin username, password and email
-    wp core install --url="localhost" --title="My WP Site" --admin_user="admin" --admin_password="adminpassword" --admin_email="admin@example.com" --allow-root
+    wp core install --url="$DOMAIN_NAME" --title="${WP_TITLE}" --admin_user="${WP_ADMIN_N}" --admin_password="${WP_ADMIN_P}" --admin_email="${WP_ADMIN_E}" --allow-root
     #create a new user with the given username, email, password and role
-    wp user create "editor" "editor@example.com" --user_pass="editorpassword" --role="editor" --allow-root
+    # mnb3d
     echo "WordPress installation complete!"
 else
     echo "WordPress is already installed."
 fi
 
-#---------------------------------------------------php config---------------------------------------------------#
 
 # change listen port from unix socket to 9000
 sed -i '36 s@/run/php/php7.4-fpm.sock@9000@' /etc/php/7.4/fpm/pool.d/www.conf
